c2c91d22448eee5a30274b7ef904d57fd119d6e5
Ahmed Abdullah <ahmed067abdullah@gmail.com>
2018-05-16 17:31:35 +0500

    Push Successful

--- comment.php
+++ comment.php
@@ -1,7 +1,6 @@
 <?php require('./header.php');?>
 
 <?php 
-	echo "test";
 	$date = getdate();
 	$stmt = $connection->prepare("INSERT INTO comments (user_id, post_id, comment,createdAt) VALUES (?, ?, ?,now())");
 	$stmt->bind_param("iis",$_SESSION['user_id'], $_POST['post_id'], $_POST['comment']);
