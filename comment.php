a6fc6cae9cf1544207ca9f5a2fb523a4e8649183
Ahmed Abdullah <ahmed067abdullah@gmail.com>
2018-05-16 15:10:16 +0500

    Test Commit

--- comment.php
+++ comment.php
@@ -1,6 +1,7 @@
 <?php require('./header.php');?>
 
 <?php 
+	echo "test";
 	$date = getdate();
 	$stmt = $connection->prepare("INSERT INTO comments (user_id, post_id, comment,createdAt) VALUES (?, ?, ?,now())");
 	$stmt->bind_param("iis",$_SESSION['user_id'], $_POST['post_id'], $_POST['comment']);
