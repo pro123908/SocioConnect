89a336c19fce02066dfafe4cd20ce50c017c1b27
Bilal Ahmad <pro123908@gmail.com>
2018-06-07 01:07:05 +0500

    Message partially designed

--- messages.php
+++ messages.php
@@ -2,7 +2,7 @@
 
 require_once('header.php');
 
-if(!isset($_SESSION['user_id'])){
+if (!isset($_SESSION['user_id'])) {
     redirection('index.php');
 }
 if (isset($_GET['id'])) {
@@ -14,8 +14,7 @@ if (isset($_GET['id'])) {
     $_SESSION['partner'] = $partnerID;
     $partner = queryFunc("select first_name from users where user_id =".$partnerID);
     $partner = isRecord($partner);
-}
-else{
+} else {
     getRecentConvo();
 }
 ?>
@@ -32,11 +31,16 @@ else{
 </div>    
 <div class="chat-box">
 <div class='chat-user'>
+    <?php if (isset($_GET['id'])) {
+    ?>
                 <span class='chat-username'><?php echo $partner['first_name'] ?></span>
+    <?php
+}?>
+    
 </div>
     <div class="convo-area">
         <?php 
-        if (isset($_GET['id'])){
+        if (isset($_GET['id'])) {
             $userID = $_SESSION['user_id'];
             $profilePicMe = getUserProfilePic($userID);
 
@@ -48,36 +52,48 @@ MESSAGE;
             echo $message;
             showMessages($partnerID);
             
-             $message = '</div>';
-            echo $message;
-            ?>
+            $message = '</div>';
+            echo $message; ?>
             <script> 
                 var last = document.querySelector(".chat-message:last-child");
                 last.scrollIntoView();
             </script> 
             <?php
+        } else {
+            $noMessage = <<<MESSAGE
+            <div class='No-message'>
+                <div class='no-message-icon'>
+                    <i class='fas fa-arrow-left'></i>
+                </div>
+                <div class='no-message-text'>
+                    <h2>No Messages</h2>
+                    <span>Search a friend to begin with</span>
+                </div> 
+               
+            </div>
+MESSAGE;
+            echo $noMessage;
         }
-        else{
-            echo "  <h2>Start a Conversation</h2><h3>&lt;==== Select Friend to start convo</h3> ";
-        } 
         ?>
         
     </div>
-    <div class='message-input-form'>
+    
         <?php
-        if (isset($_GET['id'])){
+        if (isset($_GET['id'])) {
             $messageInput = <<<DELIMETER
+            <div class='message-input-form'>
             <form method="post" name='messageForm' action="javascript:message()">
             <input autocomplete='off' name="message_body" placeholder="Type your message here"  class='message-input'></input>
             <input type='hidden' name='partner' value='$partnerID'>
             <input type='hidden' name='pic' value='$profilePicMe'>
             <input type="submit" name="send_message" id="message_submit" value="send" style='display:none'>
             </form>
+            </div>
 DELIMETER;
             echo $messageInput;
         }
         ?>
-    </div>    
+    
     
 </div>
 </div>
