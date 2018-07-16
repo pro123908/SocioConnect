7ed00b7d71fac0557cb20fc879bdd813117222d9
Bilal Ahmad <pro123908@gmail.com>
2018-07-16 22:51:17 +0500

    Change

--- includes/script.js
+++ includes/script.js
@@ -1077,14 +1077,14 @@ function showPageMessages(id, page) {
   ajaxCalls('GET', `./includes/AjaxHandlers/AJAX3.php?messagePage=1&id=${id}&page=${page}`).then(function (result) {
 
     document.querySelector(".chat-messages").innerHTML =
-      result + "<hr>"+document.querySelector(".chat-messages").innerHTML;
+      result + "<hr>" + document.querySelector(".chat-messages").innerHTML;
     document.getElementById("loading-messages").style.display = "block";
 
     if (document.getElementById("noMoreMessages").value == "true")
       document.getElementById("loading-messages").innerHTML =
         "No More Messages To Show";
     // document.getElementById("noMoreMessages").scrollIntoView();
-    document.getElementsByTagName("hr")[0].scrollIntoView();
+    document.getElementsByTagName("hr")[0].scrollIntoView({ behavior: "smooth", block: "center" });
   }).catch(function (reject) {
     // just like that
   });
