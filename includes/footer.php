d7efa12ac87769df318bf85518a060143c4dce63
Bilal Ahmad <pro123908@gmail.com>
2018-08-19 16:24:39 +0500

    merge

--- includes/footer.php
+++ includes/footer.php
@@ -22,20 +22,7 @@ $loc = substr($location,1,12);
                 <span><a class='owner-names sponsor-link contact-info' href= "mailto:ahmed067abdullah@gmail.com" >ahmed067abdullah@gmail.com</a></span>
                 <span><a class='owner-names sponsor-link contact-info' href= "mailto:bilal069@icloud.com">bilal069@icloud.com</a></span>
             <?php } 
-            else { ?> 
-                <span>
-                    <a class='owner-names sponsor-link' href="https://www.facebook.com/ahmed.abdullah.378" target="_blank">Ahmed Abdullah</a>, 
-                    <a class='owner-names sponsor-link' href="https://www.facebook.com/pro123908" target="_blank">Bilal Ahmad</a> 
-                </span>
-                <span><a class='owner-names sponsor-link contact-info' href= "mailto:pro123908@gmail.com">pro123908@gmail.com</a></span>
-                <span><a class='owner-names sponsor-link contact-info' href= "mailto:ahmed067abdullah@gmail.com" >ahmed067abdullah@gmail.com</a>
-            <?php }
         ?>
-    </div>
-    <div class='copyright'>
-        <?php if($loc != 'socioConnect') { ?>
-        Sponsored by <a href="https://www.facebook.com/abmotorskarachi/?ref=br_rs" target="_blank" class="owner-names sponsor-link">A.B Motors</a><br>
-        <?php } ?>
         &copy; 2018 Copyrights. All Rights Reserved.
     </div>
 </div>
